9bc07c382d6bc62c883201d4cd70d6553ecfffa7
Jai Gupta <126405080+jaigupta04@users.noreply.github.com>
2024-02-07 00:38:42 +0530

    Update 73.cpp

--- 73.cpp
+++ 73.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void setZeros( vector<vector<int>>& matrix ) {
+void setZeros(vector<vector<int>>& matrix) {
     int sizeRow = matrix.size();
     int sizeCol = matrix[0].size();
-    int col[sizeCol] = {-1}, row[sizeRow] = {-1};
-    
-    for( int i = 0; i < sizeRow; i++ ) {
+
+    vector<int> row(sizeRow, -1);
+    vector<int> col(sizeCol, -1);
+
+    for (int i = 0; i < sizeRow; i++) {
         int count = 0;
-        for( int j = 0; j < sizeCol; j++ ) {
-            if( matrix[i][j] == 0 ) {
+        for (int j = 0; j < sizeCol; j++) {
+            if (matrix[i][j] == 0) {
                 col[j] = j;
                 count++;
             }
         }
-        if( count>0 ) {
+        if (count > 0) {
             row[i] = i;
         }
     }
 
-    for( int i = 0; i < sizeRow; i++ ) {
-        if( i == row[i] ) {
-            for( int j = 0; j < sizeCol; j++ ) {
-                matrix[i][j] = 0;
-            }
+    for (int i = 0; i < sizeRow; i++) {
+        if (i == row[i]) {
+            fill(matrix[i].begin(), matrix[i].end(), 0);
         } else {
-            for( int j = 0; j < sizeCol; j++ ) {
-                if( j == col[j] ) matrix[i][j] = 0;
+            for (int j = 0; j < sizeCol; j++) {
+                if (j == col[j]) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
@@ -35,7 +38,15 @@ void setZeros( vector<vector<int>>& matrix ) {
 
 int main() {
     vector<vector<int>> myMatrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-    setZeros( myMatrix );
+    setZeros(myMatrix);
+
+    // Print the matrix after setting zeros
+    for (const auto& row : myMatrix) {
+        for (int val : row) {
+            cout << val << "\t";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
