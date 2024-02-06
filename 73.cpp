536cdb312ac5178a3c3282525ffc51143d38b268
Jai Gupta <126405080+jaigupta04@users.noreply.github.com>
2024-02-07 00:05:30 +0530

    Update 73.cpp

--- 73.cpp
+++ 73.cpp
@@ -2,32 +2,32 @@
 #include <vector>
 using namespace std;
 
-void setZeros(vector<vector<int>>& matrix) {
+void setZeros( vector<vector<int>>& matrix ) {
     int sizeRow = matrix.size();
     int sizeCol = matrix[0].size();
     int col[sizeCol] = {-1}, row[sizeRow] = {-1};
     
-    for(int i=0; i<sizeRow; i++){
+    for( int i = 0; i < sizeRow; i++ ) {
         int count = 0;
-        for(int j=0; j<sizeCol; j++){
-            if(matrix[i][j] == 0){
+        for( int j = 0; j < sizeCol; j++ ) {
+            if( matrix[i][j] == 0 ) {
                 col[j] = j;
                 count++;
             }
         }
-        if(count>0){
+        if( count>0 ) {
             row[i] = i;
         }
     }
 
-    for(int i=0; i<sizeRow; i++){
-        if(i == row[i]){
-            for(int j=0; j<sizeCol; j++){
+    for( int i = 0; i < sizeRow; i++ ) {
+        if( i == row[i] ) {
+            for( int j = 0; j < sizeCol; j++ ) {
                 matrix[i][j] = 0;
             }
-        } else{
-            for(int j=0; j<sizeCol; j++){
-                if(j == col[j]) matrix[i][j] = 0;
+        } else {
+            for( int j = 0; j < sizeCol; j++ ) {
+                if( j == col[j] ) matrix[i][j] = 0;
             }
         }
     }
@@ -35,7 +35,7 @@ void setZeros(vector<vector<int>>& matrix) {
 
 int main() {
     vector<vector<int>> myMatrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-    setZeros(myMatrix);
+    setZeros( myMatrix );
 
     return 0;
 }
