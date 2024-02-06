1450adcf2515a8c377a5702fcd295cdee086a42c
Jai Gupta <126405080+jaigupta04@users.noreply.github.com>
2024-02-07 00:37:23 +0530

    Update 48.cpp

--- 48.cpp
+++ 48.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void rotate( vector<vector<int>>& matrix ) {
-    
-    for( int i = 0; i < matrix.size()/2; i++ ) {
-        for( int j = i; j < matrix.size() - 1 - i; j++ ) {
-            swap( matrix[i][j], matrix[j][matrix.size() - 1 - i] );
-            swap( matrix[i][j], matrix[matrix.size() - 1 - i][matrix.size() - 1 - i - j + i] );
-            swap( matrix[i][j], matrix[matrix.size() - 1 - i - j + i][i] );
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = i; j < n - 1 - i; j++) {
+            swap(matrix[i][j], matrix[j][n - 1 - i]);
+            swap(matrix[i][j], matrix[n - 1 - i][n - 1 - i - j + i]);
+            swap(matrix[i][j], matrix[n - 1 - i - j + i][i]);
         }
     }
-    
-    for( int i = 0; i < matrix.size(); i++ ){
-        for( int j = 0; j < matrix.size(); j++ ){
+
+    // Print rotated matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << matrix[i][j] << "\t";
         }
         cout << endl;
@@ -21,8 +24,14 @@ void rotate( vector<vector<int>>& matrix ) {
 }
 
 int main() {
-    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-    rotate( matrix );
+    vector<vector<int>> matrix = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    rotate(matrix);
 
     return 0;
 }
